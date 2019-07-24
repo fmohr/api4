e1b594c0d3d3629ef5f69a325527653dc7ee2ada
Felix Mohr <fmohr@mail.upb.de>
2019-07-24 09:19:16 +0200

    Adjusted the IPath interface and the interface for uncertainty

--- ai/graphsearch/src/main/java/org/api4/java/ai/graphsearch/problem/pathsearch/pathevaluation/IUncertaintySource.java
+++ ai/graphsearch/src/main/java/org/api4/java/ai/graphsearch/problem/pathsearch/pathevaluation/IUncertaintySource.java
@@ -2,9 +2,11 @@ package org.api4.java.ai.graphsearch.problem.pathsearch.pathevaluation;
 
 import java.util.List;
 
+import org.api4.java.datastructure.graph.IPath;
+
 @FunctionalInterface
 public interface IUncertaintySource<T, A, V extends Comparable<V>> {
 
-	public double calculateUncertainty(IEvaluatedPath<T, A, V> n, List<List<T>> simulationPaths, List<V> simulationEvaluations);
+	public double calculateUncertainty(IEvaluatedPath<T, A, V> n, List<IPath<T, A>> simulationPaths, List<V> simulationEvaluations);
 
 }
--- datastructure/src/main/java/org/api4/java/datastructure/graph/IPath.java
+++ datastructure/src/main/java/org/api4/java/datastructure/graph/IPath.java
@@ -10,7 +10,19 @@ public interface IPath<N, A> {
 
 	public IPath<N, A> getPathToParentOfHead();
 
+	public IPath<N, A> getPathFromChildOfRoot();
+
 	public List<N> getNodes();
 
+	public boolean isPoint();
+
+	public int getNumberOfNodes();
+
 	public List<A> getArcs();
+
+	public A getInArc(N node);
+
+	public A getOutArc(N node);
+
+	public boolean containsNode(N node);
 }
