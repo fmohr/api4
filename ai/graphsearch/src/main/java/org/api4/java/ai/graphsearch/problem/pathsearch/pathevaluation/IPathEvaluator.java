8a572a77770cd07b036a79ba1f2f12e15cccf47a
Felix Mohr <fmohr@mail.upb.de>
2019-07-29 15:01:41 +0200

    Adjusted the path evaluator interface

--- ai/graphsearch/src/main/java/org/api4/java/ai/graphsearch/problem/pathsearch/pathevaluation/IPathEvaluator.java
+++ ai/graphsearch/src/main/java/org/api4/java/ai/graphsearch/problem/pathsearch/pathevaluation/IPathEvaluator.java
@@ -1,7 +1,9 @@
 package org.api4.java.ai.graphsearch.problem.pathsearch.pathevaluation;
 
+import org.api4.java.common.attributedobjects.IObjectEvaluator;
 import org.api4.java.datastructure.graph.IPath;
 
-public interface IPathEvaluator<N, A, V extends Comparable<V>> {
-	public V f(IPath<N, A> path) throws PathEvaluationException, InterruptedException;
+public interface IPathEvaluator<N, A, V extends Comparable<V>> extends IObjectEvaluator<IPath<N, A>, V> {
+	@Override
+	public V evaluate(IPath<N, A> path) throws PathEvaluationException, InterruptedException;
 }
--- ai/graphsearch/src/main/java/org/api4/java/ai/graphsearch/problem/pathsearch/pathevaluation/PathEvaluationException.java
+++ ai/graphsearch/src/main/java/org/api4/java/ai/graphsearch/problem/pathsearch/pathevaluation/PathEvaluationException.java
@@ -1,9 +1,9 @@
 package org.api4.java.ai.graphsearch.problem.pathsearch.pathevaluation;
 
-import org.api4.java.algorithm.exceptions.AlgorithmException;
+import org.api4.java.common.attributedobjects.ObjectEvaluationFailedException;
 
 @SuppressWarnings("serial")
-public class PathEvaluationException extends AlgorithmException {
+public class PathEvaluationException extends ObjectEvaluationFailedException {
 
 	public PathEvaluationException(final String message) {
 		super(message);
