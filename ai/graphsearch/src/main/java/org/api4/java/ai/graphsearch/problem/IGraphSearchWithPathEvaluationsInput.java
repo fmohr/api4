8e1994746a280e1d234d0323085c427e6e449198
Felix Mohr <fmohr@mail.upb.de>
2019-08-19 15:13:47 +0200

    Updated IGraphSearchWithPathEvaluationsInput

--- ai/graphsearch/src/main/java/org/api4/java/ai/graphsearch/problem/IGraphSearchWithPathEvaluationsInput.java
+++ ai/graphsearch/src/main/java/org/api4/java/ai/graphsearch/problem/IGraphSearchWithPathEvaluationsInput.java
@@ -1,8 +1,7 @@
 package org.api4.java.ai.graphsearch.problem;
 
-import org.api4.java.common.attributedobjects.IObjectEvaluator;
-import org.api4.java.datastructure.graph.IPath;
+import org.api4.java.ai.graphsearch.problem.pathsearch.pathevaluation.IPathEvaluator;
 
 public interface IGraphSearchWithPathEvaluationsInput<N, A, V extends Comparable<V>> extends IGraphSearchInput<N, A> {
-	public IObjectEvaluator<IPath<N, A>, V> getPathEvaluator();
+	public IPathEvaluator<N, A, V> getPathEvaluator();
 }
