91ce317bcf894fef1403ca007fb2ec66a70b726b
Felix <fmohr@mail.upb.de>
2019-09-23 18:57:52 +0200

    Adjusted ObjectEvaluator and PathEvaluator

--- ai/graphsearch/src/main/java/org/api4/java/ai/graphsearch/problem/pathsearch/pathevaluation/IPathEvaluator.java
+++ ai/graphsearch/src/main/java/org/api4/java/ai/graphsearch/problem/pathsearch/pathevaluation/IPathEvaluator.java
@@ -4,6 +4,7 @@ import org.api4.java.common.attributedobjects.IObjectEvaluator;
 import org.api4.java.datastructure.graph.IPath;
 
 public interface IPathEvaluator<N, A, V extends Comparable<V>> extends IObjectEvaluator<IPath<N, A>, V> {
+	
 	@Override
-	public V f(IPath<N, A> path) throws PathEvaluationException, InterruptedException;
+	public V evaluate(IPath<N, A> path) throws PathEvaluationException, InterruptedException;
 }
--- common/src/main/java/org/api4/java/common/attributedobjects/IObjectEvaluator.java
+++ common/src/main/java/org/api4/java/common/attributedobjects/IObjectEvaluator.java
@@ -1,12 +1,12 @@
 package org.api4.java.common.attributedobjects;
 
 public interface IObjectEvaluator<T, V extends Comparable<V>> extends IGetter<T, V> {
-	public V f(T object) throws InterruptedException, ObjectEvaluationFailedException;
+	public V evaluate(T object) throws InterruptedException, ObjectEvaluationFailedException;
 
 	@Override
 	default V getPropertyOf(final T object) throws GetPropertyFailedException, InterruptedException {
 		try {
-			return this.f(object);
+			return this.evaluate(object);
 		} catch (InterruptedException e) {
 			throw e;
 		} catch (ObjectEvaluationFailedException e) {
