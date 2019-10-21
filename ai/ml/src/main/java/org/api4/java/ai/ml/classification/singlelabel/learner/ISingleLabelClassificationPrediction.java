0042e26deb69420089e6746062f70d11ef187915
mwever <wever@mail.uni-paderborn.de>
2019-10-21 17:08:49 +0200

    Refactored prediction.

--- ai/ml/src/main/java/org/api4/java/ai/ml/classification/singlelabel/learner/ISingleLabelClassificationPrediction.java
+++ ai/ml/src/main/java/org/api4/java/ai/ml/classification/singlelabel/learner/ISingleLabelClassificationPrediction.java
@@ -1,5 +1,7 @@
 package org.api4.java.ai.ml.classification.singlelabel.learner;
 
+import java.util.Map;
+
 import org.api4.java.ai.ml.core.learner.algorithm.IPrediction;
 
 public interface ISingleLabelClassificationPrediction extends IPrediction {
@@ -9,7 +11,7 @@ public interface ISingleLabelClassificationPrediction extends IPrediction {
 
 	public String getLabelWithHighestProbability();
 
-	public double[] getClassDistribution();
+	public Map<String, Double> getClassDistribution();
 
 	public double getProbabilityOfLabel(String label);
 
