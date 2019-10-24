f492c42aeb4d4abc7f10c7ba64646910695ec8a5
mwever <wever@mail.uni-paderborn.de>
2019-10-24 17:45:39 +0200

    Turned dataset again into list instead of collection.

--- ai/ml/src/main/java/org/api4/java/ai/ml/core/dataset/IDataset.java
+++ ai/ml/src/main/java/org/api4/java/ai/ml/core/dataset/IDataset.java
@@ -1,18 +1,14 @@
 package org.api4.java.ai.ml.core.dataset;
 
-import java.util.Collection;
+import java.util.List;
 
 import org.api4.java.ai.ml.core.dataset.schema.attribute.IAttribute;
 import org.api4.java.ai.ml.core.exception.DatasetCreationException;
 
-public interface IDataset<I extends IInstance> extends IDataSource<I>, Collection<I> {
+public interface IDataset<I extends IInstance> extends IDataSource<I>, List<I> {
 
 	@Override
 	public IDataset<I> createEmptyCopy() throws DatasetCreationException, InterruptedException;
-
-	public I get(int pos);
-
-	public void add(int index, I instance);
 
 	public Object[][] getFeatureMatrix();
 
