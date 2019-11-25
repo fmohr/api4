0ab7475d883a5946992b0ac6a80ad664ba2d5bb8
mwever <wever@mail.uni-paderborn.de>
2019-11-25 14:43:44 +0100

    Completed interface of the Multi-Label Classifier.

--- ai/ml/src/main/java/org/api4/java/ai/ml/classification/multilabel/learner/IMultiLabelClassifier.java
+++ ai/ml/src/main/java/org/api4/java/ai/ml/classification/multilabel/learner/IMultiLabelClassifier.java
@@ -1,7 +1,31 @@
 package org.api4.java.ai.ml.classification.multilabel.learner;
 
 import org.api4.java.ai.ml.classification.IClassifier;
+import org.api4.java.ai.ml.classification.multilabel.evaluation.IMultiLabelClassification;
+import org.api4.java.ai.ml.classification.multilabel.evaluation.IMultiLabelClassificationPredictionBatch;
+import org.api4.java.ai.ml.core.dataset.supervised.ILabeledDataset;
+import org.api4.java.ai.ml.core.dataset.supervised.ILabeledInstance;
+import org.api4.java.ai.ml.core.exception.PredictionException;
+import org.api4.java.ai.ml.core.exception.TrainingException;
 
 public interface IMultiLabelClassifier extends IClassifier {
+
+	@Override
+	public IMultiLabelClassification fitAndPredict(ILabeledDataset<ILabeledInstance> dTrain, ILabeledInstance xTest) throws TrainingException, PredictionException, InterruptedException;
+
+	@Override
+	public IMultiLabelClassificationPredictionBatch fitAndPredict(ILabeledDataset<ILabeledInstance> dTrain, ILabeledInstance[] xTest) throws TrainingException, PredictionException, InterruptedException;
+
+	@Override
+	public IMultiLabelClassificationPredictionBatch fitAndPredict(ILabeledDataset<ILabeledInstance> dTrain, ILabeledDataset<ILabeledInstance> dTest) throws TrainingException, PredictionException, InterruptedException;
+
+	@Override
+	public IMultiLabelClassification predict(ILabeledInstance xTest) throws PredictionException, InterruptedException;
+
+	@Override
+	public IMultiLabelClassificationPredictionBatch predict(ILabeledDataset<ILabeledInstance> dTest) throws PredictionException, InterruptedException;
+
+	@Override
+	public IMultiLabelClassificationPredictionBatch predict(ILabeledInstance[] dTest) throws PredictionException, InterruptedException;
 
 }
