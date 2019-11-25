9e2ae24c6d7a9a0f19234c4112b63c02fc09afc9
mwever <wever@mail.uni-paderborn.de>
2019-11-25 16:22:17 +0100

    Removed dataset builder.

--- ai/ml/src/main/java/org/api4/java/ai/ml/core/dataset/IDatasetBuilder.java
+++
@@ -1,8 +0,0 @@
-package org.api4.java.ai.ml.core.dataset;
-
-import org.api4.java.ai.ml.core.dataset.descriptor.IDatasetDescriptor;
-import org.api4.java.ai.ml.core.exception.DatasetCreationException;
-
-public interface IDatasetBuilder {
-	public IDataset<? extends IInstance> getDataset(IDatasetDescriptor descriptor) throws DatasetCreationException;
-}
