1467bd656589b5966bedb5d7ea80476a25333791
Felix <fmohr@mail.upb.de>
2019-12-09 22:26:37 +0100

    Modified LearnerRunReport to include exceptions.

--- ai/ml/src/main/java/org/api4/java/ai/ml/core/evaluation/execution/ILearnerRunReport.java
+++ ai/ml/src/main/java/org/api4/java/ai/ml/core/evaluation/execution/ILearnerRunReport.java
@@ -1,15 +1,22 @@
 package org.api4.java.ai.ml.core.evaluation.execution;
 
+import org.api4.java.ai.ml.core.dataset.supervised.ILabeledDataset;
 import org.api4.java.ai.ml.core.evaluation.IPredictionAndGroundTruthTable;
 
 public interface ILearnerRunReport {
-	public int getTrainingTimeInMS();
-	
-	public int getTestTimeInMS();
-	
-	public String getTrainSetDescriptor();
-	
-	public String getTestSetDescriptor();
-	
+	public long getTrainStartTime();
+
+	public long getTrainEndTime();
+
+	public long getTestStartTime();
+
+	public long getTestEndTime();
+
+	public Throwable getException();
+
+	public ILabeledDataset<?> getTrainSet();
+
+	public ILabeledDataset<?> getTestSet();
+
 	public IPredictionAndGroundTruthTable getPredictionDiffList();
 }
--- ai/ml/src/main/java/org/api4/java/ai/ml/core/evaluation/execution/LearnerExecutionFailedException.java
+++ ai/ml/src/main/java/org/api4/java/ai/ml/core/evaluation/execution/LearnerExecutionFailedException.java
@@ -3,16 +3,61 @@ package org.api4.java.ai.ml.core.evaluation.execution;
 public class LearnerExecutionFailedException extends Exception {
 
 	private static final long serialVersionUID = 8952892481193885473L;
-	
-	public LearnerExecutionFailedException(Exception e) {
-		super(e);
+
+	private final long trainTimeStart;
+	private final long trainTimeEnd;
+	private final long testTimeStart;
+	private final long testTimeEnd;
+
+	public LearnerExecutionFailedException(final long trainTimeStart, final long trainTimeEnd, final Exception e) {
+		this(trainTimeStart, trainTimeEnd, -1, -1, e);
 	}
 
-	public LearnerExecutionFailedException(String message, Exception e) {
-		super(message, e);
+	public LearnerExecutionFailedException(final long trainTimeStart, final long trainTimeEnd, final String message, final Exception e) {
+		this(trainTimeStart, trainTimeEnd, -1, -1, message, e);
 	}
-	
-	public LearnerExecutionFailedException(String message) {
+
+	public LearnerExecutionFailedException(final long trainTimeStart, final long trainTimeEnd, final String message) {
+		this(trainTimeStart, trainTimeEnd, -1, -1, message);
+	}
+
+	public LearnerExecutionFailedException(final long trainTimeStart, final long trainTimeEnd, final long testTimeStart, final long testTimeEnd, final Exception e) {
+		super(e);
+		this.trainTimeStart = trainTimeStart;
+		this.trainTimeEnd = trainTimeEnd;
+		this.testTimeStart = testTimeStart;
+		this.testTimeEnd = testTimeEnd;
+	}
+
+	public LearnerExecutionFailedException(final long trainTimeStart, final long trainTimeEnd, final long testTimeStart, final long testTimeEnd, final String message, final Exception e) {
+		super(message, e);
+		this.trainTimeStart = trainTimeStart;
+		this.trainTimeEnd = trainTimeEnd;
+		this.testTimeStart = testTimeStart;
+		this.testTimeEnd = testTimeEnd;
+	}
+
+	public LearnerExecutionFailedException(final long trainTimeStart, final long trainTimeEnd, final long testTimeStart, final long testTimeEnd, final String message) {
 		super(message);
+		this.trainTimeStart = trainTimeStart;
+		this.trainTimeEnd = trainTimeEnd;
+		this.testTimeStart = testTimeStart;
+		this.testTimeEnd = testTimeEnd;
+	}
+
+	public long getTrainTimeStart() {
+		return this.trainTimeStart;
+	}
+
+	public long getTrainTimeEnd() {
+		return this.trainTimeEnd;
+	}
+
+	public long getTestTimeStart() {
+		return this.testTimeStart;
+	}
+
+	public long getTestTimeEnd() {
+		return this.testTimeEnd;
 	}
 }
